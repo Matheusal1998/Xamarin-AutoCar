c7ddcdc80f9546aa0db22ed517224acf19039d0e
Matheus Lima <matheus.al1998@gmail.com>
2020-02-17 23:24:48 -0300

    Iniciando versonamento

---
+++ RunCar/Detalhes.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RunCar
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Detalhes
+    {
+
+        public const int freio_abs = 500;
+        public const int ar_condicionado = 1000;
+        public const int mp3 = 300;
+
+        bool TemFreioAbs;
+        bool TemArCondicionado;
+        bool TemMP3;
+
+        public bool temArCondionado{
+
+            get{ return TemArCondicionado;}
+            set{
+                TemArCondicionado = value;
+                if (TemArCondicionado)
+                    DisplayAlert("Ar condicionado", "Ligado!", "Ok");
+                else
+                    DisplayAlert("Ar condicionado", "Desligado!", "Ok");
+
+
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+
+        }
+
+        public bool temMP3
+        {
+            get { return TemMP3; }
+            set
+            {
+                TemMP3 = value;
+
+                if (TemMP3)
+                    DisplayAlert("MP3", "Ligado!", "Ok");
+                else
+                    DisplayAlert("MP3", "Desligado", "Ok!");
+
+
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+        public bool temFreioAbs
+        {
+            get { return TemFreioAbs; 
+            
+            }
+            set
+            {
+                TemFreioAbs = value;
+                if (TemFreioAbs)
+                {
+                    DisplayAlert("Freio ABS", "Ligado!", "Ok");
+                  
+                }
+                else
+                {
+                    DisplayAlert("Freio ABS", "Deligado!", "Ok");
+                   
+                }
+
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        public string ValorTotal
+        {
+            get
+            {
+                return string.Format("Valor Total: R$ {0}", 
+                    (temFreioAbs? freio_abs : 0)+
+                    (temArCondionado ? ar_condicionado : 0)+
+                    (temMP3? mp3 : 0)+
+                    Veiculo.preco
+                    );
+            }
+        }
+
+        public string TextFreio
+        {
+            get
+            {
+                return string.Format("Freio ABS - R$ {0} ", freio_abs);
+            }
+        }
+        public string TextAr
+        {
+            get
+            {
+                return string.Format("Ar condicionado - R$ {0}", ar_condicionado);
+            }
+        }
+        public string TextMP3
+        {
+            get
+            {
+                return string.Format("MP3 Player - R$ {0}", mp3);
+            }
+        }
+        public Veiculo Veiculo { get; set; }
+        public Detalhes(Veiculo veiculo)
+        {
+            this.Veiculo = veiculo;
+            InitializeComponent();
+            this.BindingContext = this;
+
+
+
+
+        }
+
+        private void botaoProximo_Clicked(object sender, EventArgs e)
+        {
+
+            Navigation.PushAsync(new Agendamento( this.Veiculo));
+        }
+       
+    }
+}
---
+++ RunCar/Listagem.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RunCar
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+
+    public class Veiculo
+    {
+        public string nome { get; set; }
+        public decimal preco { get; set; }
+        public string PrecoFormatado
+        {
+            get
+            {
+                return string.Format("R$ {0}", preco);
+            }
+        }
+
+        public FormattedString VeiculoLabel
+        {
+            get
+            {
+                return new FormattedString
+                {
+                    Spans = {
+                        new Span { Text = nome },
+                        new Span { Text = " - " },
+                        new Span { Text = PrecoFormatado, FontAttributes = FontAttributes.Bold } }
+                };
+            }
+            set { }
+        }
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        public List<Veiculo> Veiculos { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            this.Veiculos = new List<Veiculo>()
+            {
+                new Veiculo { nome = "Celta 1.0", preco = 9000 },
+                new Veiculo { nome = "Ideia 1.4", preco = 2700 },
+                new Veiculo { nome = "Gran Siena 2.0", preco = 20000 },
+                new Veiculo { nome = "C3 1.0", preco = 22000 },
+                new Veiculo { nome = "Uno Fire", preco = 11000 },
+                new Veiculo { nome = "Sentra 2.0", preco = 53000 },
+                new Veiculo { nome = "Astra Sedan", preco = 39000 },
+                new Veiculo { nome = "Vectra 2.0 Turbo", preco = 37000 },
+                new Veiculo { nome = "Hilux 4x4", preco = 90000 },
+                new Veiculo { nome = "Montana Cabine dupla", preco = 57000 },
+                new Veiculo { nome = "Outlander 2.4", preco = 99000 },
+                new Veiculo { nome = "Brasilia Amarela", preco = 9500 },
+                new Veiculo { nome = "Omega Hatch", preco = 8000 }
+            };
+
+            this.BindingContext = this;
+        }
+        private void listViewVeiculos_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var veiculo = (Veiculo)e.Item;
+
+            Navigation.PushAsync(new Detalhes(veiculo));
+        }
+
+    }
+}
